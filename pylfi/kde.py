c0d08de01cc5afc5ffd9ac49f5ab7b02d1dd6fd8
Nicolai Haug <nicolaihaug@yahoo.com>
2021-03-20 14:23:59 +0100

    improved grid search plots

--- pylfi/kde.py
+++ pylfi/kde.py
@@ -13,6 +13,22 @@ from sklearn.neighbors import KernelDensity
 '''
 perhaps scrap sklearn and use this instead:
 https://github.com/tommyod/KDEpy
+
+sklearn:
+https://github.com/scikit-learn/scikit-learn/blob/95119c13af77c76e150b753485c662b7c52a41a2/sklearn/neighbors/_kde.py
+https://jakevdp.github.io/PythonDataScienceHandbook/05.13-kernel-density-estimation.html
+https://stackoverflow.com/questions/53135857/what-does-rank-test-score-stand-for-from-the-model-cv-results
+
+performance comparison:
+https://kdepy.readthedocs.io/en/latest/comparison.html
+
+other resources:
+----------------
+https://stats.stackexchange.com/questions/90656/kernel-bandwidth-scotts-vs-silvermans-rules
+http://www.stat.cmu.edu/~cshalizi/350/lectures/28/lecture-28.pdf
+https://stats.stackexchange.com/questions/173637/generating-a-sample-from-epanechnikovs-kernel
+https://github.com/statsmodels/statsmodels/blob/master/statsmodels/nonparametric/bandwidths.py
+https://stackoverflow.com/questions/30145957/plotting-2d-kernel-density-estimation-with-python
 '''
 
 
@@ -51,7 +67,7 @@ class KDE:
 
         if isinstance(self._bw, str):
             if self._bw == "auto":
-                self._bw = np.logspace(-4, 1, 100)
+                self._bw = np.logspace(-2, 1, 100)
             elif self._bw == "scott":
                 self._bw = [self._scotts_bw_rule()]
             elif self._bw == "silverman":
@@ -88,7 +104,7 @@ class KDE:
                             # scoring='mutual_info_score',
                             # scoring=_scoring,
                             # cv=LeaveOneOut(),
-                            # cv=5
+                            cv=5
                             )
         return grid
 
@@ -162,16 +178,48 @@ class KDE:
         scores_std = np.array(scores_std).reshape(
             len(self._kernel), len(self._bw))
 
-        fig, ax = plt.subplots(1, 1)
+        scores_rank = self._grid_scores['rank_test_score']
+        scores_rank = np.array(scores_rank).reshape(
+            len(self._kernel), len(self._bw))
+
+        fig, ax = plt.subplots(1, 3, figsize=(14, 4))
 
         for idx, kernel_name in enumerate(self._kernel):
-            ax.plot(np.log10(self._bw),
-                    scores_mean[idx, :], '-', label=kernel_name)
-        ax.set_title("Grid Search Scores")
-        ax.set_xlabel("Bandwidth")
-        ax.set_ylabel('CV Average Score')
-        ax.legend(loc="best")
-        ax.grid('on')
+            ax[0].plot(self._bw, scores_mean[idx, :],
+                       label=kernel_name + " kernel")
+            ax[0].set_xscale('log')
+            ax[0].set_yscale('symlog')
+            ax[0].set_ylabel('CV Mean Score')
+            ax[0].set_xlabel("Bandwidth")
+            ax[0].grid('on')
+
+            ax[1].plot(self._bw, scores_std[idx, :],
+                       label=kernel_name + " kernel")
+            ax[1].set_xscale('log')
+            ax[1].set_yscale('log')
+            ax[1].set_ylabel('CV Standard Deviation Score')
+            ax[1].set_xlabel("Bandwidth")
+            ax[1].grid('on')
+
+            ax[2].plot(self._bw, scores_rank[idx, :] / 100,
+                       label=kernel_name + " kernel")
+            ax[2].set_xscale('log')
+            ax[2].set_ylabel('CV Rank Score')
+            ax[2].set_xlabel("Bandwidth")
+            ax[2].grid('on')
+
+        #handles, labels = ax.get_legend_handles_labels()
+        handles, labels = plt.gca().get_legend_handles_labels()
+        plt.legend(handles,
+                   labels,
+                   loc='center left',
+                   bbox_to_anchor=(1.04, 0.5),
+                   fancybox=True,
+                   borderaxespad=0.1,
+                   ncol=1
+                   )
+        fig.suptitle("Grid Search Scores")
+        plt.grid('on')
 
 
 # Maybe include
@@ -195,7 +243,6 @@ if __name__ == "__main__":
 
     import matplotlib.pyplot as plt
     import scipy.stats as stats
-    from sklearn.decomposition import PCA
 
     def make_data(N, f=0.3, rseed=1):
         rand = np.random.RandomState(rseed)
@@ -209,36 +256,58 @@ if __name__ == "__main__":
         obs_data = likelihood.rvs(size=N)
         return obs_data
 
-    data = make_data2(1000)
+    ###
+    ###
+    np.random.seed(42)
+    N = int(5e3)
+    groundtruth = 2.0
+    likelihood = stats.norm(loc=0., scale=np.sqrt(groundtruth))
+    data = likelihood.rvs(size=N)
 
-    # observed data
+    #data = make_data2(1000)
 
-    x = np.linspace(-4, 10, 1000)
     #kde = KDE(data, bandwidth='auto', kernel='auto')
-    kde = KDE(data, bandwidth='auto', kernel=[
-              'gaussian', 'epanechnikov'])
+    #kde = KDE(data, bandwidth='auto', kernel=['gaussian', 'tophat'])
+    kde = KDE(data, bandwidth='auto', kernel='gaussian')
 
     best_params = kde.best_params
     print('Optimal params:', best_params)
 
     grid_scores = kde.grid_scores
     kde.plot_grid_search()
-    #plt.ylim(-0.5, 0.5)
+    plt.tight_layout()
     plt.show()
-    # print(grid_scores)
+
+    # print(grid_scores.keys())
     #plot.grid_search(grid_scores, change='n_estimators', kind='bar')
 
-    density = kde.density(x)
-    density2 = kde(x)
-    kde_samples = kde.sample(1000)
+    x = np.linspace(-5, 5, int(1e4))
+    density = kde(x)
+    kde_samples = kde.sample(int(1e4))
+
+    true_pdf = likelihood.pdf(x)
 
     sample_mean = np.mean(data)
+    sample_median = np.median(data)
+    sample_std = np.std(data)
     kde_mean = np.mean(kde_samples)
-    print('sample mean:', sample_mean)
-    print('kde mean:', kde_mean)
-    plt.figure()
-    plt.hist(data, density=True)
-    plt.plot(x, density, color='r')
-    plt.axvline(kde_mean, color='k', ls=':')
-    plt.axvline(sample_mean, color='k', ls='--')
+    kde_median = np.median(kde_samples)
+    kde_std = np.std(kde_samples)
+    print(f'sample mean: {sample_mean:.4f}')
+    print(f'sample median: {sample_median:.4f}')
+    print(f'sample std: {sample_std:.4f}')
+    print(f'kde mean: {kde_mean:.4f}')
+    print(f'kde median: {kde_median:.4f}')
+    print(f'kde std: {kde_std:.4f}')
+
+    fig, ax = plt.subplots(1, 1)
+    ax.hist(data, density=True, histtype='bar', alpha=0.5, label='hist')
+    ax.plot(x, density, color='r', label='kde')
+    ax.plot(x, true_pdf, color='k', label='true pdf')
+    ax.axvline(kde_mean, ymax=0.3, color='r', label='kde mean')
+    ax.axvline(0, ymax=0.3, color='k', ls=':', label='true mean')
+    ax.set_ylabel('Density')
+    ax.set_xlabel('$x$')
+    #ax.axvline(sample_mean, label='data mean')
+    ax.legend()
     plt.show()
