90e9c59b2d976257880c8c7818efe85b1c7fc620
Nicolai Haug <nicolaihaug@yahoo.com>
2021-03-31 04:06:17 +0200

    Class representation of the Hodgkin-Huxley model

--- models/hodgkin_huxley.py
+++ models/hodgkin_huxley.py
@@ -281,7 +281,7 @@ class HodgkinHuxley:
 
         # solve HH ODEs
         solution = solve_ivp(self, t_span=(0, T), y0=y0,
-                             t_eval=t_eval, first_step=dt, max_step=5 * dt, **kwargs)
+                             t_eval=t_eval, first_step=dt, **kwargs)
 
         # store solutions
         self._time = solution.t
@@ -450,90 +450,3 @@ class HodgkinHuxley:
             return self._h
         except AttributeError:
             raise ODEsNotSolved("Missing call to solve. No solution exists.")
-
-
-if __name__ == "__main__":
-    import matplotlib as mpl
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-    from matplotlib import gridspec
-    from stimulus import constant_stimulus
-
-    # plt.style.use('seaborn')
-    sns.set()
-    sns.set_context("paper")
-    sns.set_style("darkgrid", {"axes.facecolor": "0.96"})
-
-    # Set fontsizes in figures
-    params = {'legend.fontsize': 'large',
-              'axes.labelsize': 'large',
-              'axes.titlesize': 'large',
-              'xtick.labelsize': 'large',
-              'ytick.labelsize': 'large',
-              'legend.fontsize': 'large',
-              'legend.handlelength': 2}
-    plt.rcParams.update(params)
-    plt.rc('text', usetex=True)
-
-    # remove top and right axis from plots
-    mpl.rcParams['axes.spines.right'] = False
-    mpl.rcParams['axes.spines.top'] = False
-
-    # simulation parameters
-    T = 50.
-    dt = 0.025
-    I_amp = 0.32
-    r_soma = 40
-    threshold = -55  # AP threshold
-
-    # input stimulus
-    stimulus = constant_stimulus(
-        I_amp=I_amp, T=T, dt=dt, t_stim_on=10, t_stim_off=40, r_soma=r_soma)
-    # print(stimulus["info"])
-    I = stimulus["I"]
-    I_stim = stimulus["I_stim"]
-
-    # HH simulation
-    hh = HodgkinHuxley()
-
-    hh.V_rest = np.array([0, 1])
-
-    hh.solve(I, T, [dt])
-    t = hh.t
-    V = hh.V
-    n = hh.n
-    m = hh.m
-    h = hh.h
-
-    # plot voltage trace
-    fig = plt.figure(figsize=(8, 7), tight_layout=True, dpi=120)
-    gs = gridspec.GridSpec(3, 1, height_ratios=[4, 4, 1])
-    ax = plt.subplot(gs[0])
-    plt.plot(t, V, lw=2)
-    plt.text(0.01, 0.25, 'Threshold', fontsize=12,
-             color='darkred', transform=plt.gca().transAxes)
-    plt.axhline(threshold, xmax=0.253, ls=':', color='darkred')
-    plt.ylabel('Voltage (mV)')
-    ax.set_xticks([])
-    ax.set_yticks([-80, -55, -20, 10, 40])
-
-    ax = plt.subplot(gs[1])
-    plt.plot(t, n, '-.', lw=1.5, label='$n$')
-    plt.plot(t, m, "--", lw=1.5, label='$m$')
-    plt.plot(t, h, ls=':', lw=1.5, label='$h$')
-    plt.legend(loc='upper right')
-    plt.ylabel("State")
-    ax.set_xticks([])
-    ax.set_yticks([0.0, 0.25, 0.5, 0.75, 1.0])
-
-    ax = plt.subplot(gs[2])
-    plt.plot(t, I_stim, 'k', lw=2)
-    plt.xlabel('Time (ms)')
-    plt.ylabel('Stimulus (nA)')
-
-    ax.set_xticks([0, 10, 25, 40, np.max(t)])
-    ax.set_yticks([0, np.max(I_stim)])
-    ax.yaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%.2f'))
-
-    fig.suptitle("Hodgkin-Huxley Model")
-    plt.show()
