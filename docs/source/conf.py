f3df741657bb515e7d4b6a4844709498397c3e2f
Nicolai Haug <nicolaihaug@yahoo.com>
2021-04-09 17:56:19 +0200

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,6 +74,9 @@ napoleon_use_ivar = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# generate autosummary even if no references
+autosummary_generate = True
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
