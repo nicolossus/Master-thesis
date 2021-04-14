c0dbbb124698a1670fb19eef0a32f27a224fcfb6
Nicolai Haug <nicolaihaug@yahoo.com>
2021-04-15 01:28:16 +0200

    rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -301,6 +301,17 @@ htmlhelp_basename = 'pylfidoc'
 #html_theme = "sphinx_rtd_theme"
 html_theme = "pydata_sphinx_theme"
 
+html_context = {
+    "display_github": True,
+    "github_host": "github.com",
+    "gitlab_user": "nicolossus",
+    "gitlab_repo": 'master-thesis',
+    "gitlab_version": "HEAD",
+    "conf_py_path": "/docs/source/",
+    "source_suffix": '.rst'
+}
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
